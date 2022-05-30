80d4f67753823ff8f93b363276f5be684e10026c
NyaNyaa <90991026+Nyaou@users.noreply.github.com>
2022-05-30 19:23:43 -0300

    Add files via upload

---
+++ interior-consultant-master/menu-lateral.js
@@ -0,0 +1,9 @@
+var open = document.getElementById('open-menu')
+var menu = document.getElementById('menu')
+var button = document.getElementById('button-nav')
+
+function openMenu(){
+    
+}
+
+open.addEventListener('click',openMenu)
